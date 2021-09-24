3ab5a3e2519a026d550b3cf02ab8564bfc3a0371
simonych <simonych@gmail.com>
2021-09-24 13:26:29 +0300

    make

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const restify = require('restify');
 
+//////////////////////////////////////////////////////
+const pinoms = require('pino-multi-stream')
+const childProcess = require('child_process')
+const stream = require('stream')
+
+const cwd = process.cwd()
+const { env } = process
+
+const logThrough = new stream.PassThrough()
+const prettyStream = pinoms.prettyStream()
+const streams = [
+  { stream: logThrough },
+  { stream: prettyStream }
+]
+const log = pinoms(pinoms.multistream(streams))
+
+const child = childProcess.spawn(process.execPath, [
+  require.resolve('pino-tee'),
+  'info', `${__dirname}/log/info`,
+  'warn', `${__dirname}/log/warn`,
+  'error', `${__dirname}/log/error`,
+  'fatal', `${__dirname}/log/fatal`
+], { cwd, env })
+
+logThrough.pipe(child.stdin)
+////////////////////////////////////////////////////
+
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
+  log.info(JSON.stringify(req.params, null, '  '));
   next();
 }
 
@@ -10,5 +38,5 @@ server.get('/log/:name', respond);
 server.head('/log/:name', respond);
 
 server.listen(5145, function() {
-  console.log('%s listening at %s', server.name, server.url);
+  log.info('%s listening at %s', server.name, server.url);
 });
